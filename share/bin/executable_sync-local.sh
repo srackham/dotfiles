6e8873a48826d3f5ae49cd7d82a81b2fc80385d3
Stuart Rackham <srackham@gmail.com>
2024-11-04 13:44:58 +1300

    add `sync-local.sh` to mirror local projects to server

---
+++ share/bin/executable_sync-local.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#
+# Mirror top-level projects in ~/local/projects directories to ~/share/projects
+#
+
+# Source and destination directories
+SRC="$HOME/local/projects"
+DEST="$HOME/share/projects"
+
+# Get a list of top-level directories in the source
+top_level_dirs=$(find "$SRC" -maxdepth 1 -type d -printf '%P\n' | grep -v '^$')
+
+# Sync each top-level directory individually
+for dir in $top_level_dirs; do
+    rsync -avz --delete "$SRC/$dir/" "$DEST/$dir/"
+done
