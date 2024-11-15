cd486edf58a7ef4f16bdb90d81cdb3ca2b932ce2
Stuart Rackham <srackham@gmail.com>
2024-11-15 15:41:39 +1300

    wezterm: usable configuration

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,12 +2,11 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.font = wezterm.font 'JetBrains Mono'
--- config.color_scheme = 'Catppuccin Macchiato'
--- config.color_scheme = 'catppuccin-mocha'
--- config.color_scheme = 'Chalk'
-config.color_scheme = 'Classic Dark (base16)'
+config.color_scheme = 'Argonaut (Gogh)'
 config.font_size = 10.0
 config.initial_rows = 50
 config.initial_cols = 120
+config.audible_bell = "Disabled"
+config.enable_tab_bar = false
 
 return config
