ba4f748e81b0a439b0220aefad584865d69f2edf
Stuart Rackham <srackham@gmail.com>
2024-12-16 14:05:06 +1300

    wezterm: fix post NixOS 24.11 upgraded compatibility issue

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -9,5 +9,6 @@ config.initial_cols = 120
 config.audible_bell = "Disabled"
 config.enable_tab_bar = false
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- Disable ligatures
+config.front_end = "WebGpu"                                 -- https://github.com/NixOS/nixpkgs/issues/336069#issuecomment-2306768055
 
 return config
