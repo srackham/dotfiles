8c8237135cae90bcdbd38b4143f86eb89b3d69db
Stuart Rackham <srackham@gmail.com>
2024-12-16 19:11:47 +1300

    nvim: mappings for Latin long vowels and common glyphs

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -12,12 +12,25 @@ vim.keymap.set('i', '<C-v>', '<C-r>+', { noremap = true, silent = true })
 -- Map U to redo.
 vim.keymap.set('n', 'U', '<C-r>', { noremap = true, silent = true })
 
-vim.keymap.set('n', '<leader><leader>', '<C-^>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>c', ':update | bd<cr>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>q', ':wqa<cr>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>w', ':wa<cr>', { noremap = true, silent = true })
-vim.keymap.set('i', '<A-S-d>', '<C-r>=strftime("%d-%b-%Y")<cr>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader><Leader>', '<C-^>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>c', ':update | bd<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>q', ':wqa<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>w', ':wa<CR>', { noremap = true, silent = true })
+vim.keymap.set('i', '<A-S-d>', '<C-r>=strftime("%d-%b-%Y")<CR>', { noremap = true, silent = true })
 
 -- Spell checker mappings (https://neovim.io/doc/user/spell.html)
-vim.keymap.set('n', '<leader>s', ':set invspell<cr>', { noremap = true, silent = true }) -- Toggle spell checker
-vim.keymap.set('i', '<C-l>', '<esc>[s1z=`]a', { noremap = true, silent = true })         -- Correct last error
+vim.keymap.set('n', '<Leader>s', ':set invspell<CR>', { noremap = true, silent = true }) -- Toggle spell checker
+vim.keymap.set('i', '<C-l>', '<Esc>[s1z=`]a', { noremap = true, silent = true })         -- Correct last error
+
+-- Latin long vowels
+vim.keymap.set('i', '<C-Bslash>a', 'ā', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-Bslash>e', 'ē', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-Bslash>i', 'ī', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-Bslash>o', 'ō', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-Bslash>u', 'ū', { noremap = true, silent = true })
+
+-- UTF8 characters
+vim.keymap.set('i', '<C-Bslash>d', '†', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-Bslash>h', '…', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-Bslash>m', '—', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-Bslash>v', '⋮', { noremap = true, silent = true })
