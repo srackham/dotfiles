f5715aabeb07e1cde72597e9a66b1e8a3dd64881
Stuart Rackham <srackham@gmail.com>
2024-12-20 17:04:05 +1300

    nvim: drop redundant empty `opts` variable

--- dot_config/nvim/lua/plugins/lsp-config.lua
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -18,7 +18,6 @@ return {
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require "lspconfig"
-      local opts = {}
       lspconfig.gopls.setup({
         capabilities = capabilities,
       })
@@ -31,16 +30,16 @@ return {
       lspconfig.gleam.setup({
         capabilities = capabilities,
       })
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-      vim.keymap.set('n', '<Leader>lr', vim.lsp.buf.rename, opts)
-      vim.keymap.set('n', '<Leader>la', vim.lsp.buf.code_action, opts)
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help)
+      vim.keymap.set('n', '<Leader>lr', vim.lsp.buf.rename)
+      vim.keymap.set('n', '<Leader>la', vim.lsp.buf.code_action)
       vim.keymap.set('n', '<Leader>lf', function()
         vim.lsp.buf.format { async = true }
-      end, opts)
+      end)
     end,
   },
 }
