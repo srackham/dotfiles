94d6d15174254d4b8395fe11919ef1564f99f7a3
Stuart Rackham <srackham@gmail.com>
2024-12-21 17:48:33 +1300

    nvim: add gitsigns plugin key mappings

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -1,10 +1,16 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require("gitsigns").setup()
-    vim.keymap.set("n", "<Leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
-    vim.keymap.set("n", "<Leader>gp", ":Gitsigns preview_hunk<CR>", {})
-    vim.keymap.set("n", "<Leader>gr", ":Gitsigns reset_hunk<CR>", {})
+    local gitsigns = require('gitsigns')
+    gitsigns.setup()
+    vim.keymap.set('n', '<Leader>gb', gitsigns.toggle_current_line_blame)
+    vim.keymap.set('n', '<Leader>gd', gitsigns.diffthis)
+    vim.keymap.set('n', '<Leader>gp', gitsigns.preview_hunk)
+    vim.keymap.set('n', '<Leader>gR', gitsigns.reset_buffer)
+    vim.keymap.set('n', '<Leader>gr', gitsigns.reset_hunk)
+    vim.keymap.set('n', '<Leader>gS', gitsigns.stage_buffer)
+    vim.keymap.set('n', '<Leader>gs', gitsigns.stage_hunk)
+    vim.keymap.set('n', '<Leader>gu', gitsigns.undo_stage_hunk)
     vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { fg = "#999999" })
   end,
 }
