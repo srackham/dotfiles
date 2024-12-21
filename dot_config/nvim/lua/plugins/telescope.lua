69b369e3f3571c6377e4abc95f1767ad791573b0
Stuart Rackham <srackham@gmail.com>
2024-12-22 10:20:34 +1300

    nvim: map `tF` to Telescope find files by extension function

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -7,10 +7,22 @@ return {
     },
     config = function()
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<Leader>f', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<Leader>b', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<Leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<Leader>?', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', 'tf', builtin.find_files, { desc = 'Telescope find files' })
+      vim.keymap.set('n', 'tb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', 'tg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', 'th', builtin.help_tags, { desc = 'Telescope help tags' })
+      local ext = ''
+      vim.keymap.set('n', 'tF', function()
+        vim.ui.input({ prompt = "Enter file name extension: ", default = ext }, function(input)
+          if input then
+            ext = input
+            builtin.find_files({
+              prompt_title = "Find ." .. ext .. " Files",
+              find_command = { 'rg', '--files', '--hidden', '--glob', '**/*.' .. ext }
+            })
+          end
+        end)
+      end, { desc = 'Telescope filtered find files' })
     end,
   },
   {
