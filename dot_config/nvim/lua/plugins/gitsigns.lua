99994b2cbcec7cccc5d0df267a0724f7ab07f6e3
Stuart Rackham <srackham@gmail.com>
2024-12-22 18:55:27 +1300

    nvim: remap gitsigns commands and add change navigation key bindings

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -3,14 +3,33 @@ return {
   config = function()
     local gitsigns = require('gitsigns')
     gitsigns.setup()
-    vim.keymap.set('n', '<Leader>gb', gitsigns.toggle_current_line_blame)
-    vim.keymap.set('n', '<Leader>gd', gitsigns.diffthis)
-    vim.keymap.set('n', '<Leader>gp', gitsigns.preview_hunk)
-    vim.keymap.set('n', '<Leader>gR', gitsigns.reset_buffer)
-    vim.keymap.set('n', '<Leader>gr', gitsigns.reset_hunk)
-    vim.keymap.set('n', '<Leader>gS', gitsigns.stage_buffer)
-    vim.keymap.set('n', '<Leader>gs', gitsigns.stage_hunk)
-    vim.keymap.set('n', '<Leader>gu', gitsigns.undo_stage_hunk)
+
+    -- Navigation
+    vim.keymap.set('n', ']g', function()
+      if vim.wo.diff then
+        vim.cmd.normal({ ']g', bang = true })
+      else
+        gitsigns.nav_hunk('next')
+      end
+    end)
+
+    vim.keymap.set('n', '[g', function()
+      if vim.wo.diff then
+        vim.cmd.normal({ '[g', bang = true })
+      else
+        gitsigns.nav_hunk('prev')
+      end
+    end)
+
+    -- Actions
+    vim.keymap.set('n', '<Leader>gb', gitsigns.toggle_current_line_blame, { desc = "Toggle line blame" })
+    vim.keymap.set('n', '<Leader>gd', gitsigns.diffthis, { desc = "Diff hunk" })
+    vim.keymap.set('n', '<Leader>gp', gitsigns.preview_hunk, { desc = "Preview hunk" })
+    vim.keymap.set('n', '<Leader>gR', gitsigns.reset_buffer, { desc = "Reset all hunks in current buffer" })
+    vim.keymap.set('n', '<Leader>gr', gitsigns.reset_hunk, { desc = "Reset hunk at cursor" })
+    vim.keymap.set('n', '<Leader>gS', gitsigns.stage_buffer, { desc = "Stage all hunks in current buffer" })
+    vim.keymap.set('n', '<Leader>gs', gitsigns.stage_hunk, { desc = "Stage hunk at cursor" })
+    vim.keymap.set('n', '<Leader>gu', gitsigns.undo_stage_hunk, { desc = "Undo staged hunk at cursor" })
     vim.api.nvim_set_hl(0, 'GitSignsCurrentLineBlame', { fg = '#999999' })
   end,
 }
