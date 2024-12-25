e52520c7180cd0f839f6184711ecc2e69ed91cfb
Stuart Rackham <srackham@gmail.com>
2024-12-25 22:15:15 +1300

    nvim: add custom key mappings for common window commands

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -12,17 +12,21 @@ vim.keymap.set('n', '<C-v>', '"+Pl', { noremap = true, silent = true })
 vim.keymap.set('v', '<C-v>', 'd"+Pl', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-v>', '<C-r>+', { noremap = true, silent = true })
 
--- Map U to redo.
-vim.keymap.set('n', 'U', '<C-r>', { noremap = true, silent = true })
-
-vim.keymap.set('n', '<Leader><Leader>', '<C-^>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Leader>w', ':update<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<Leader>c', ':update | bd<CR>', { noremap = true, silent = false })
-vim.keymap.set('n', '<Leader>n', ':enew | w ++p ', { noremap = true, silent = false })
-vim.keymap.set('i', '<A-S-d>', '<C-r>=strftime("%d-%b-%Y")<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'U', '<C-r>', { noremap = true, silent = true, desc = "Redo last change" })
+vim.keymap.set('n', '<Leader>n', ':enew | w ++p ', { noremap = true, silent = false, desc = "New file" })
+vim.keymap.set('i', '<A-S-d>', '<C-r>=strftime("%d-%b-%Y")<CR>', { noremap = true, silent = true, desc = "Insert date" })
+vim.keymap.set('n', '<Leader><Leader>', '<C-^>',
+  { noremap = true, silent = true, desc = "Go to previously edited buffer" })
 
 -- Spell checker mappings (https://neovim.io/doc/user/spell.html)
 vim.keymap.set('n', '<Leader>s', ':set invspell<CR>', { noremap = true, silent = true }) -- Toggle spell checker
+
+-- Windows commands
+vim.keymap.set('n', '<Leader>wc', ':q<CR>', { noremap = true, silent = false, desc = "Close window" })
+vim.keymap.set('n', '<Leader>wm', '<C-w>_', { noremap = true, silent = false, desc = "Maximize window" })
+vim.keymap.set('n', '<Leader>wo', '<C-w>o', { noremap = true, silent = false, desc = "Only this window" })
+vim.keymap.set('n', '<Leader>wu', ':update<CR>', { noremap = true, silent = false, desc = "Update window" })
+vim.keymap.set('n', '<Leader>ww', '<C-w>w', { noremap = true, silent = false, desc = "Next window" })
 
 -- Latin long vowels
 vim.keymap.set('i', '<C-Bslash>a', 'ā', { noremap = true, silent = true })
