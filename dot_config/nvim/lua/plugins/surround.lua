5ceefd6c7fdbaafee722e8d3583d3636403c0d68
Stuart Rackham <srackham@gmail.com>
2024-12-26 11:39:03 +1300

    nvim: add ,sa ,sd ,sc custom surround plugin key mappings

--- dot_config/nvim/lua/plugins/surround.lua
+++ dot_config/nvim/lua/plugins/surround.lua
@@ -3,5 +3,11 @@ return {
   event = 'VeryLazy',
   config = function()
     require('nvim-surround').setup()
+    vim.keymap.set('v', '<Leader>sa', '<Plug>(nvim-surround-visual)',
+      { noremap = false, silent = true, desc = "Surround selection with character" })
+    vim.keymap.set('n', '<Leader>sd', '<Plug>(nvim-surround-delete)',
+      { noremap = false, silent = true, desc = "Delete surrounding characters" })
+    vim.keymap.set('n', '<Leader>sc', '<Plug>(nvim-surround-change)',
+      { noremap = false, silent = true, desc = "Change surrounding characters" })
   end
 }
