bb10ef234c5389474015566c22a8ffc7fc673c07
Stuart Rackham <srackham@gmail.com>
2024-12-25 19:29:10 +1300

    nvim: add ,fk key mapping for Telescope keymaps command

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -7,11 +7,12 @@ return {
     },
     config = function()
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<Leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<Leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<Leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<Leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-      vim.keymap.set('n', '<Leader>fs', builtin.spell_suggest, { desc = 'Spelling Suggestions' })
+      vim.keymap.set('n', '<Leader>ff', builtin.find_files, { desc = "Telescope find files" })
+      vim.keymap.set('n', '<Leader>fb', builtin.buffers, { desc = "Telescope buffers" })
+      vim.keymap.set('n', '<Leader>fg', builtin.live_grep, { desc = "Telescope live grep" })
+      vim.keymap.set('n', '<Leader>fh', builtin.help_tags, { desc = "Telescope help tags" })
+      vim.keymap.set('n', '<Leader>fs', builtin.spell_suggest, { desc = "Spelling Suggestions" })
+      vim.keymap.set('n', '<Leader>fk', builtin.keymaps, { desc = "Normal mode key mappings" })
 
       local ext = ''
       local function map_extension_filter(cmd, desc, callback)
