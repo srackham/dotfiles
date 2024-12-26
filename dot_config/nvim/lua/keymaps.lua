3ddbe965db3ae1b859c6da6064552d834b527725
Stuart Rackham <srackham@gmail.com>
2024-12-27 08:11:52 +1300

    nvim: add key mappings: <Space>, <A-Space> for page down and up; ,ws window splt; ,w= equalize windows

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,5 +1,13 @@
 vim.keymap.set('n', '<A-v>', '<C-v>', { noremap = true, silent = true, desc = "Enter visual block mode" })
 vim.keymap.set('n', '<Esc><Esc>', ':nohlsearch<CR>', { silent = true, desc = "Turn highlighing off" })
+vim.keymap.set('n', 'U', '<C-r>', { noremap = true, silent = true, desc = "Redo last change" })
+vim.keymap.set('n', '<Leader>n', ':enew | w ++p ', { noremap = true, silent = false, desc = "New file" })
+vim.keymap.set('i', '<A-S-d>', '<C-r>=strftime("%d-%b-%Y")<CR>', { noremap = true, silent = true, desc = "Insert date" })
+vim.keymap.set('n', '<Leader><Leader>', '<C-^>',
+  { noremap = true, silent = true, desc = "Go to previously edited buffer" })
+vim.keymap.set('n', '<Leader>S', ':set invspell<CR>', { noremap = true, silent = true, desc = "Toggle spell checker" })
+vim.keymap.set('n', '<Space>', '<C-f>', { noremap = true, silent = true })
+vim.keymap.set('n', '<A-Space>', '<C-b>', { noremap = true, silent = true })
 
 -- Map Ctrl+C to copy to the clipboard
 vim.keymap.set('v', '<C-c>', '"+y', { noremap = true, silent = true, desc = "Copy selection to clipboard" })
@@ -10,21 +18,14 @@ vim.keymap.set('n', '<C-v>', '"+Pl', { noremap = true, silent = true, desc = "Pa
 vim.keymap.set('v', '<C-v>', 'd"+Pl', { noremap = true, silent = true, desc = "Paste clipboard" })
 vim.keymap.set('i', '<C-v>', '<C-r>+', { noremap = true, silent = true, desc = "Paste clipboard" })
 
-vim.keymap.set('n', 'U', '<C-r>', { noremap = true, silent = true, desc = "Redo last change" })
-vim.keymap.set('n', '<Leader>n', ':enew | w ++p ', { noremap = true, silent = false, desc = "New file" })
-vim.keymap.set('i', '<A-S-d>', '<C-r>=strftime("%d-%b-%Y")<CR>', { noremap = true, silent = true, desc = "Insert date" })
-vim.keymap.set('n', '<Leader><Leader>', '<C-^>',
-  { noremap = true, silent = true, desc = "Go to previously edited buffer" })
-
--- Spell checker mappings (https://neovim.io/doc/user/spell.html)
-vim.keymap.set('n', '<Leader>S', ':set invspell<CR>', { noremap = true, silent = true, desc = "Toggle spell checker" })
-
 -- Windows commands
 vim.keymap.set('n', '<Leader>wc', ':q<CR>', { noremap = true, silent = false, desc = "Close window" })
 vim.keymap.set('n', '<Leader>wm', '<C-w>_', { noremap = true, silent = false, desc = "Maximize window" })
 vim.keymap.set('n', '<Leader>wo', '<C-w>o', { noremap = true, silent = false, desc = "Only this window" })
+vim.keymap.set('n', '<Leader>ws', ':split', { noremap = true, silent = false, desc = "Split window horizontally" })
 vim.keymap.set('n', '<Leader>wu', ':update<CR>', { noremap = true, silent = false, desc = "Update window" })
 vim.keymap.set('n', '<Leader>ww', '<C-w>w', { noremap = true, silent = false, desc = "Next window" })
+vim.keymap.set('n', '<Leader>w=', '<C-w>=', { noremap = true, silent = false, desc = "Equalize window sizes" })
 
 -- Latin long vowels
 vim.keymap.set('i', '<C-Bslash>a', 'ā', { noremap = true, silent = true })
