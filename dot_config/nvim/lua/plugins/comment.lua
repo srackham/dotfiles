6914877d6f222975b0b0c0825c135015a182c1b2
Stuart Rackham <srackham@gmail.com>
2024-12-26 15:59:58 +1300

    nvim:  key mappings use `Ctrl+/` for line-wise comments and `Alt+/` for block-wise comments

--- dot_config/nvim/lua/plugins/comment.lua
+++ dot_config/nvim/lua/plugins/comment.lua
@@ -2,28 +2,14 @@ return {
   'numToStr/Comment.nvim',
   config = function()
     require('Comment').setup({
+      toggler = {
+        line = '<C-_>',  -- Line-comment toggle keymap
+        block = '<A-/>', -- Block-comment toggle keymap
+      },
       opleader = {
-        line = '<Leader>cl',  -- Line-comment keymap
-        block = '<Leader>cb', -- Block-comment keymap
+        line = '<C-_>',  -- Line-comment keymap
+        block = '<A-/>', -- Block-comment keymap
       },
     })
-
-    vim.keymap.set('n', '<C-_>', function()
-      local count = vim.v.count
-      local api = require('Comment.api')
-      if count == 0 then
-        api.toggle.linewise.current()
-      else
-        api.toggle.linewise.count(count)
-      end
-    end, { noremap = true, silent = true, desc = "Toggle line comments (with optional count)" })
   end,
-
-  vim.keymap.set('x', '<C-_>', function()
-    local api = require('Comment.api')
-    local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
-    vim.api.nvim_feedkeys(esc, 'nx', false)
-    api.toggle.linewise(vim.fn.visualmode())
-  end, { noremap = true, silent = true, desc = "Toggle line comments for selection" })
-
 }
