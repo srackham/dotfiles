b83fffa8e6ac371201ace26515010cc3ac70f7fa
Stuart Rackham <srackham@gmail.com>
2025-01-04 16:27:00 +1300

    nvim: add `,dr` mapping to restart the debugging session

--- dot_config/nvim/lua/plugins/debugging.lua
+++ dot_config/nvim/lua/plugins/debugging.lua
@@ -31,10 +31,12 @@ return {
     vim.keymap.set('n', '<Leader>di', dap_cmd(dap.step_into), { desc = "Debug step into" })
     vim.keymap.set('n', '<Leader>dj', dap_cmd(dap.focus_frame), { desc = "Jump to debug cursor" })
     vim.keymap.set('n', '<Leader>do', dap_cmd(dap.step_out), { desc = "Debug step out" })
+    vim.keymap.set('n', '<Leader>dr', dap_cmd(dap.step_last), { desc = "Restart the debug session" })
     vim.keymap.set('n', '<Leader>ds', dap_cmd(dap.set_breakpoint), { desc = "Debug set breakpoint" })
     vim.keymap.set('n', '<Leader>dt', dap_cmd(dap.toggle_breakpoint), { desc = "Debug toggle breakpoint" })
     vim.keymap.set('n', '<Leader>du', dapui.toggle, { desc = "Debug toggle UI" })
-    vim.keymap.set('n', '<Leader>dU', function() dapui.open({ reset = true }) end, { desc = "Open and reset debug UI" })
+    vim.keymap.set('n', '<Leader>dU', function() dapui.open({ reset = true }) end,
+      { desc = "Open and reinitialize the debug UI" })
     vim.keymap.set('n', '<Leader>dx', dap.terminate, { desc = "Debug terminate" })
 
     vim.fn.sign_define('DapBreakpoint', { text = '●', texthl = 'DapBreakpoint' })
