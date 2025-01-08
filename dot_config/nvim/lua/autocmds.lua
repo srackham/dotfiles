4479d519c825e2b38ff4cc0e0aa0e06b2ce69436
Stuart Rackham <srackham@gmail.com>
2025-01-08 21:38:44 +1300

    nvim: refactor markdown and text autocmds

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -18,10 +18,15 @@ vim.api.nvim_create_autocmd('InsertLeave', {
 })
 
 -- Enable spell checking for markdown and text files by default
-vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-  pattern = { '*.md', '*.txt' },
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  pattern = { 'markdown', 'text' },
   callback = function()
     vim.opt_local.spell = true
+    -- Soft-wrapped line navigation for markdown and text files
+    vim.keymap.set('n', 'j', 'gj', { buffer = true })
+    vim.keymap.set('n', 'k', 'gk', { buffer = true })
+    vim.keymap.set('n', '0', 'g0', { buffer = true })
+    vim.keymap.set('n', '$', 'g$', { buffer = true })
   end
 })
 
@@ -30,16 +35,5 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
 vim.api.nvim_create_autocmd({ 'FileType' }, {
   callback = function()
     vim.cmd('set formatoptions-=ro')
-  end,
-})
-
--- Soft-wrapped line navigation for markdown and text files
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'markdown', 'text' },
-  callback = function()
-    vim.keymap.set('n', 'j', 'gj', { buffer = true })
-    vim.keymap.set('n', 'k', 'gk', { buffer = true })
-    vim.keymap.set('n', '0', 'g0', { buffer = true })
-    vim.keymap.set('n', '$', 'g$', { buffer = true })
   end,
 })
