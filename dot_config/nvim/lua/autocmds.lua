a09c2a0d45fa709d9117ea470dcfe0157cf53af6
Stuart Rackham <srackham@gmail.com>
2025-01-08 21:19:45 +1300

    nvim: Soft-wrapped line navigation for markdown and text files

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -17,7 +17,7 @@ vim.api.nvim_create_autocmd('InsertLeave', {
   end,
 })
 
--- Enable spell checking for .md and .txt files by default
+-- Enable spell checking for markdown and text files by default
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = { '*.md', '*.txt' },
   callback = function()
@@ -30,5 +30,16 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
 vim.api.nvim_create_autocmd({ 'FileType' }, {
   callback = function()
     vim.cmd('set formatoptions-=ro')
+  end,
+})
+
+-- Soft-wrapped line navigation for markdown and text files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'markdown', 'text' },
+  callback = function()
+    vim.keymap.set('n', 'j', 'gj', { buffer = true })
+    vim.keymap.set('n', 'k', 'gk', { buffer = true })
+    vim.keymap.set('n', '0', 'g0', { buffer = true })
+    vim.keymap.set('n', '$', 'g$', { buffer = true })
   end,
 })
