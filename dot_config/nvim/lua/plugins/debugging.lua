8de30020a6c1e93986027b97cd9641c04c92b68c
Stuart Rackham <srackham@gmail.com>
2025-01-08 14:02:13 +1300

    nvim: drop `n`/`N` step over/continue mappings when debugging

--- dot_config/nvim/lua/plugins/debugging.lua
+++ dot_config/nvim/lua/plugins/debugging.lua
@@ -16,23 +16,14 @@ return {
     dap.listeners.before.event_terminated.dapui_config = dapui.close
     dap.listeners.before.event_exited.dapui_config = dapui.close
 
-    -- Map `n` and `N` when a debug command is executed.
-    local function dap_cmd(cmd)
-      return function()
-        vim.keymap.set('n', 'n', dap.step_over)
-        vim.keymap.set('n', 'N', dap.continue)
-        cmd()
-      end
-    end
-
-    vim.keymap.set('n', '<Leader>dc', dap_cmd(dap.continue), { desc = "Debug continue" })
-    vim.keymap.set('n', '<Leader>dC', dap_cmd(dap.clear_breakpoints), { desc = "Clear all breakpoints" })
-    vim.keymap.set('n', '<Leader>dd', dap_cmd(dap.step_over), { desc = "Debug step over" })
-    vim.keymap.set('n', '<Leader>di', dap_cmd(dap.step_into), { desc = "Debug step into" })
-    vim.keymap.set('n', '<Leader>dj', dap_cmd(dap.focus_frame), { desc = "Jump to debug cursor" })
-    vim.keymap.set('n', '<Leader>do', dap_cmd(dap.step_out), { desc = "Debug step out" })
-    vim.keymap.set('n', '<Leader>dr', dap_cmd(dap.step_last), { desc = "Restart the debug session" })
-    vim.keymap.set('n', '<Leader>db', dap_cmd(dap.toggle_breakpoint), { desc = "Debug toggle breakpoint" })
+    vim.keymap.set('n', '<Leader>dc', dap.continue, { desc = "Debug continue" })
+    vim.keymap.set('n', '<Leader>dC', dap.clear_breakpoints, { desc = "Clear all breakpoints" })
+    vim.keymap.set('n', '<Leader>dd', dap.step_over, { desc = "Debug step over" })
+    vim.keymap.set('n', '<Leader>di', dap.step_into, { desc = "Debug step into" })
+    vim.keymap.set('n', '<Leader>dj', dap.focus_frame, { desc = "Jump to debug cursor" })
+    vim.keymap.set('n', '<Leader>do', dap.step_out, { desc = "Debug step out" })
+    vim.keymap.set('n', '<Leader>dr', dap.restart, { desc = "Restart the debug session" })
+    vim.keymap.set('n', '<Leader>db', dap.toggle_breakpoint, { desc = "Debug toggle breakpoint" })
     vim.keymap.set('n', '<Leader>du', dapui.toggle, { desc = "Debug toggle UI" })
     vim.keymap.set('n', '<Leader>dU', function() dapui.open({ reset = true }) end,
       { desc = "Open and reinitialize the debug UI" })
