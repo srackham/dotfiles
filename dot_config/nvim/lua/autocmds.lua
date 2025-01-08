0d73f2177df2df00379c50cab579dace72e192fd
Stuart Rackham <srackham@gmail.com>
2025-01-08 21:39:45 +1300

    nvim: drop unnecessary comment

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -17,7 +17,6 @@ vim.api.nvim_create_autocmd('InsertLeave', {
   end,
 })
 
--- Enable spell checking for markdown and text files by default
 vim.api.nvim_create_autocmd({ 'FileType' }, {
   pattern = { 'markdown', 'text' },
   callback = function()
