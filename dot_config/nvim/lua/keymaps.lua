062f8233e999971bc0b79f6202336ed735c353d2
Stuart Rackham <srackham@gmail.com>
2025-01-10 15:37:49 +1300

    nvim: `,wc` and `,C` mappings close window. Drop unused `,wu`(window update mapping

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -80,11 +80,11 @@ vim.keymap.set('v', '<C-v>', 'd"+Pl', { noremap = true, silent = true, desc = "P
 vim.keymap.set('i', '<C-v>', '<C-r>+', { noremap = true, silent = true, desc = "Paste clipboard" })
 
 -- Windows commands
-vim.keymap.set('n', '<Leader>wc', ':update | bdelete<CR>', { noremap = true, silent = false, desc = "Close window" })
+vim.keymap.set('n', '<Leader>wc', ':close<CR>', { noremap = true, silent = false, desc = "Close window" })
+vim.keymap.set('n', '<Leader>C', ':close<CR>', { noremap = true, silent = false, desc = "Close window" })
 vim.keymap.set('n', '<Leader>wC', '<C-w>o', { noremap = true, silent = true, desc = "Leave only this window open" })
 vim.keymap.set('n', '<Leader>wm', '<C-w>_', { noremap = true, silent = true, desc = "Maximize window" })
 vim.keymap.set('n', '<Leader>wo', ':split<CR>', { noremap = true, silent = true, desc = "Open new window" })
-vim.keymap.set('n', '<Leader>wu', ':update<CR>', { noremap = true, silent = true, desc = "Save window" })
 vim.keymap.set('n', '<Leader>w=', '<C-w>=', { noremap = true, silent = true, desc = "Equalize window sizes" })
 
 -- Tab commands
