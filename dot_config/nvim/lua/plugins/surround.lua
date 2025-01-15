a146be3cc39da8ef132288f1dd8b0ef551b43036
Stuart Rackham <srackham@gmail.com>
2025-01-15 15:49:43 +1300

    nvim: add normal mode `S` surround mapping; drop redundant `,ss`, `,sv` mappings

--- dot_config/nvim/lua/plugins/surround.lua
+++ dot_config/nvim/lua/plugins/surround.lua
@@ -3,10 +3,8 @@ return {
   event = 'VeryLazy',
   config = function()
     require('nvim-surround').setup()
-    vim.keymap.set('n', '<Leader>ss', '<Plug>(nvim-surround-normal)',
+    vim.keymap.set('n', 'S', '<Plug>(nvim-surround-normal)',
       { noremap = false, silent = true, desc = "Surround text object with character" })
-    vim.keymap.set('v', '<Leader>sv', '<Plug>(nvim-surround-visual)',
-      { noremap = false, silent = true, desc = "Surround selection with character" })
     vim.keymap.set('n', '<Leader>sd', '<Plug>(nvim-surround-delete)',
       { noremap = false, silent = true, desc = "Delete surrounding characters" })
     vim.keymap.set('n', '<Leader>sc', '<Plug>(nvim-surround-change)',
