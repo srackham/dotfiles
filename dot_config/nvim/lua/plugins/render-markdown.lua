9dbbe092cd454af392ea817a3f02d95f970834c5
Stuart Rackham <srackham@gmail.com>
2025-01-18 16:31:38 +1300

    nvim: refactor render-markdown plugin header color configuration

--- dot_config/nvim/lua/plugins/render-markdown.lua
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -3,24 +3,22 @@ return {
   opts = {},
   config = function()
     local render_markdown = require('render-markdown')
-    vim.api.nvim_set_hl(0, '@markup.heading.1.markdown', { fg = '#7CA7FE', bold = true })
-    vim.api.nvim_set_hl(0, 'RenderMarkdownH1', { fg = '#7CA7FE', bold = true })
-    vim.api.nvim_set_hl(0, 'RenderMarkdownH1Bg', { bg = '#23273B' })
-    vim.api.nvim_set_hl(0, '@markup.heading.2.markdown', { fg = '#D2AB72', bold = true })
-    vim.api.nvim_set_hl(0, 'RenderMarkdownH2', { fg = '#D2AB72', bold = true })
-    vim.api.nvim_set_hl(0, 'RenderMarkdownH2Bg', { bg = '#2D282C' })
-    vim.api.nvim_set_hl(0, '@markup.heading.3.markdown', { fg = '#9FCB71', bold = true })
-    vim.api.nvim_set_hl(0, 'RenderMarkdownH3', { fg = '#9FCB71', bold = true })
-    vim.api.nvim_set_hl(0, 'RenderMarkdownH3Bg', { bg = '#252C2C' })
-    vim.api.nvim_set_hl(0, '@markup.heading.4.markdown', { fg = '#19BC9C', bold = true })
-    vim.api.nvim_set_hl(0, 'RenderMarkdownH4', { fg = '#19BC9C', bold = true })
-    vim.api.nvim_set_hl(0, 'RenderMarkdownH4Bg', { bg = '#182931' })
-    vim.api.nvim_set_hl(0, '@markup.heading.5.markdown', { fg = '#B89AF0', bold = true })
-    vim.api.nvim_set_hl(0, 'RenderMarkdownH5', { fg = '#B89AF0', bold = true })
-    vim.api.nvim_set_hl(0, 'RenderMarkdownH5Bg', { bg = '#29273A' })
-    vim.api.nvim_set_hl(0, '@markup.heading.6.markdown', { fg = '#9B7DD4', bold = true })
-    vim.api.nvim_set_hl(0, 'RenderMarkdownH6', { fg = '#9B7DD4', bold = true })
-    vim.api.nvim_set_hl(0, 'RenderMarkdownH6Bg', { bg = '#262336' })
+
+
+    local header_colors = {
+      { '#7CA7FE', '#23273B' },
+      { '#D2AB72', '#2D282C' },
+      { '#9FCB71', '#252C2C' },
+      { '#19BC9C', '#182931' },
+      { '#B89AF0', '#29273A' },
+      { '#9B7DD4', '#262336' },
+    }
+    for i = 1, 6 do
+      local fg, bg = header_colors[math.min(i, #header_colors)][1], header_colors[math.min(i, #header_colors)][2]
+      vim.api.nvim_set_hl(0, '@markup.heading.' .. i .. '.markdown', { fg = fg, bold = true })
+      vim.api.nvim_set_hl(0, 'RenderMarkdownH' .. i, { fg = fg, bold = true })
+      vim.api.nvim_set_hl(0, 'RenderMarkdownH' .. i .. 'Bg', { bg = bg })
+    end
     render_markdown.setup({
       html = {
         comment = {
