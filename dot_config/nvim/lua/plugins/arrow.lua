43e06c2e2ba0b088078c4751a982e33d55bf767a
Stuart Rackham <srackham@gmail.com>
2025-02-05 15:01:16 +1300

    nvim: install arrow.nvim bookmarks plugin

---
+++ dot_config/nvim/lua/plugins/arrow.lua
@@ -0,0 +1,11 @@
+return {
+  "otavioschwanck/arrow.nvim",
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons" },
+  },
+  opts = {
+    show_icons = true,
+    leader_key = ';',        -- File bookmarks
+    buffer_leader_key = 'm', -- Buffer bookmarks
+  }
+}
