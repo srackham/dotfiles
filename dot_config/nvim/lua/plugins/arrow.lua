99630df8f285d0f3e729f47721c136b8c29def21
Stuart Rackham <srackham@gmail.com>
2025-02-07 10:56:36 +1300

    nvim: Use `M` for buffer bookmarks

--- dot_config/nvim/lua/plugins/arrow.lua
+++ dot_config/nvim/lua/plugins/arrow.lua
@@ -1,11 +1,15 @@
 return {
-  "otavioschwanck/arrow.nvim",
+  'otavioschwanck/arrow.nvim',
   dependencies = {
-    { "nvim-tree/nvim-web-devicons" },
+    { 'nvim-tree/nvim-web-devicons' },
   },
   opts = {
     show_icons = true,
     leader_key = ';',        -- File bookmarks
-    buffer_leader_key = 'm', -- Buffer bookmarks
+    buffer_leader_key = 'M', -- Buffer bookmarks
+    separate_save_and_remove = false,
+    mappings = {
+      toggle = 'M',
+    },
   }
 }
