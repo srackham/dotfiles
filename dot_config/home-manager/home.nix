76907aaab85da26c32fa1abb9e090aea44e3044e
Stuart Rackham <srackham@gmail.com>
2025-05-03 09:16:30 +1200

    nix: add Home Manager configuration and configuration files

---
+++ dot_config/home-manager/home.nix
@@ -0,0 +1,53 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "super";
+  home.homeDirectory = "/home/super";
+  home.stateVersion = "24.11";
+
+  home.packages = with pkgs; [
+    bat
+    chezmoi
+    delve
+    deno
+    eza
+    fd
+    fzf
+    gcc
+    gh
+    git
+    gnumake
+    go
+    golangci-lint
+    golangci-lint-langserver
+    gopls
+    gotests
+    gotools
+    htop
+    jq
+    lazygit
+    lua
+    marksman
+    neovim
+    nil
+    nixfmt-rfc-style
+    nodejs
+    npm-check-updates
+    prettierd
+    ripgrep
+    sd
+    tesseract
+    tmux
+    tree
+    unzip
+    xclip
+    zoxide
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
