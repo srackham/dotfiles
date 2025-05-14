4bc319ee7d21550d936ed6e629b2d0f7e5477096
Stuart Rackham <srackham@gmail.com>
2025-05-14 17:37:09 +1200

    nvim: the `,mw` wrap command prompts for the wrap column

---
+++ dot_config/nvim/lua/plugins/floating-input.lua
@@ -0,0 +1,5 @@
+return {
+  'https://github.com/srackham/floating-input.nvim',
+  enabled = true,
+}
+
--- dot_config/nvim/lua/plugins/markdown-blocks.lua
+++ dot_config/nvim/lua/plugins/markdown-blocks.lua
@@ -4,16 +4,20 @@ return {
   version = '*', -- Install latest tagged version
   config = function()
     local mb = require('markdown-blocks')
+    local wrap_column = 50
     vim.keymap.set({ 'n', 'v' }, '<Leader>mb', mb.break_block,
       { noremap = true, silent = true, desc = "Break/unbreak the paragraph/selection at the cursor" })
     vim.keymap.set({ 'n', 'v' }, '<Leader>mq', mb.quote_block,
       { noremap = true, silent = true, desc = "Quote/unquote paragraph/selection at the cursor" })
-    vim.keymap.set({ 'n', 'v' }, '<Leader>mw', mb.wrap_block,
-      { noremap = true, silent = true, desc = "Wrap paragraph/selection at the cursor column" })
-    vim.keymap.set({ 'n', 'v' }, '<Leader>mW', function()
-      local col = tonumber(vim.fn.input('Wrap at column: '))
-      if col then mb.wrap_block(col) end
-    end, { noremap = true, silent = true, desc = "Prompted wrap paragraph/selection" })
+    vim.keymap.set({ 'n', 'v' }, '<Leader>mw', function()
+      require('floating-input').input({ prompt = "Wrap column number: ", default = tostring(wrap_column) }, function(col)
+        col = tonumber(col)
+        if col then
+          mb.wrap_block(col)
+          wrap_column = col
+        end
+      end, {})
+    end, { noremap = true, silent = true, desc = "Wrap paragraph/selection" })
     vim.keymap.set({ 'n', 'v' }, '<Leader>mu', mb.unwrap_block,
       { noremap = true, silent = true, desc = "Unwrap paragraph/selection" })
     vim.keymap.set({ 'n', 'v' }, '<Leader>mn', mb.number_block,
