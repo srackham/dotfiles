cf49f54a840de60e65c224fd5b185ccd57457a5e
Stuart Rackham <srackham@gmail.com>
2025-05-22 15:02:37 +1200

    wezterm: configure to display images in tmux

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,6 +1,12 @@
-local wezterm = require 'wezterm'
-local config = {}
+-- Basic WezTerm configuration (see https://wezterm.org/config/files.html)
 
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
 config.font = wezterm.font 'JetBrains Mono'
 config.color_scheme = 'Argonaut (Gogh)'
 config.font_size = 10.0
@@ -8,7 +14,6 @@ config.initial_rows = 50
 config.initial_cols = 120
 config.audible_bell = 'Disabled'
 config.enable_tab_bar = false
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- Disable ligatures
-config.front_end = 'WebGpu'                                 -- https://github.com/NixOS/nixpkgs/issues/336069#issuecomment-2306768055
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } -- Disable ligatures (https://wezterm.org/config/font-shaping.html)
 
 return config
