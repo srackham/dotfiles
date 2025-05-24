5752b27f71ac192f2c886e6f8187c68eb6612ff9
Stuart Rackham <srackham@gmail.com>
2025-05-24 15:27:52 +1200

    nvim: disabled telekasten.nvim plugin

--- dot_config/nvim/lua/plugins/telekasten.lua
+++ dot_config/nvim/lua/plugins/telekasten.lua
@@ -1,7 +1,7 @@
 return {
   'renerocksai/telekasten.nvim',
   dependencies = { 'nvim-telescope/telescope.nvim' },
-  enabled = true,
+  enabled = false, -- 24-May-2025: disabled because it interfered with Markdown syntax highlighting
   config = function()
     require('telekasten').setup({
       home = vim.fn.expand("~/doc"), -- Put the name of your notes directory here
