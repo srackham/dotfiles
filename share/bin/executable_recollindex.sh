c981b8a06d664df2afd6411a456a90f7d0e3e9e1
Stuart Rackham <srackham@gmail.com>
2025-05-28 09:55:45 +1200

    update recollindex.sh and sync-local.sh

--- share/bin/executable_recollindex.sh
+++ share/bin/executable_recollindex.sh
@@ -1,28 +1,23 @@
 #!/usr/bin/env bash
+#
+# Wrapper for recollindex so it generates a log file.
+#
 
-#
-# Mirror top-level projects in ~/local/projects directories to ~/share/projects
-#
+set -u  # No unbound variables.
+# set -e  # Exit on error.
+#set -x  # Echo commands
+
+PATH=/etc/profiles/per-user/srackham/bin:$PATH  # NixOS installed user binaries
 
 BASE_NAME="${0##*/}"          # Script name
 BASE_NOEXT="${BASE_NAME%.*}"  # Script name without extension
 LOGFILE="$HOME/bin/$BASE_NOEXT.log"
 DATE="date +%Y-%m-%d-%a\ %H:%M:%S"
 
-SRC="$HOME/local/projects"
-DEST="$HOME/share/projects"
+export RECOLL_CONFDIR=$HOME/.config/recoll
 
 echo $(hostname): $(eval $DATE): Starting $0 | tee --append "$LOGFILE"
-
-# Get a list of top-level directories in the source
-top_level_dirs=$(find "$SRC" -maxdepth 1 -type d -printf '%P\n' | grep -v '^$')
-
-# Sync each top-level directory individually
-for dir in $top_level_dirs; do
-    cmd="rsync -avz --delete "$SRC/$dir/" "$DEST/$dir/""
-    echo $cmd | tee --append "$LOGFILE"
-    rsync -avz --delete "$SRC/$dir/" "$DEST/$dir/"
-done
+(recollindex -k 2>&1) >/dev/null  # Drop the noisy output.
 exit_code=$?
 if [ $exit_code -eq 0 ]; then
     echo $(hostname): $(eval $DATE): Finished $0 | tee --append "$LOGFILE"
