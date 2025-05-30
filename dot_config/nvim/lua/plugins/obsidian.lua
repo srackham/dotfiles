4b2e87c70d798a812b410cb35768144f799275fc
Stuart Rackham <srackham@gmail.com>
2025-05-30 21:22:06 +1200

    nvim: move to the obsidian-nvim/obsidian.nvim community fork of obsidian.nvim
    Because it supports blink.cmp and is being more actively maintained.

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -1,18 +1,22 @@
 return {
-  "epwalsh/obsidian.nvim",
-  enabled = false,
-  version = "*", -- recommended, use latest release instead of latest commit
+  "obsidian-nvim/obsidian.nvim", -- A community fork of epwalsh/obsidian.nvim
+  version = "*",                 -- recommended, use latest release instead of latest commit
   lazy = true,
   ft = "markdown",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    'nvim-telescope/telescope.nvim', -- Optional, for search and quick-switch functionality.
   },
   opts = {
     workspaces = {
       {
         name = "doc",
-        path = "~/doc",
+        path = "~/share/doc",
       },
+    },
+    completion = {
+      blink = true,  -- Enables completion using blink.cmp
+      min_chars = 2, -- Trigger completion at 2 chars.
     },
   },
 }
