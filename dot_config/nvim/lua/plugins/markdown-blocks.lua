c0a383040155357d561b3e01b1039ba7d6c1a5b1
Stuart Rackham <srackham@gmail.com>
2025-06-19 15:02:10 +1200

    nvim: update to new markdown-blocks.nvim API names in key mappings

--- dot_config/nvim/lua/plugins/markdown-blocks.lua
+++ dot_config/nvim/lua/plugins/markdown-blocks.lua
@@ -9,11 +9,11 @@ return {
       callback = function()
         local mb = require('markdown-blocks')
         local wrap_column = 100
-        vim.keymap.set({ 'n', 'v' }, '<Leader>mb', mb.break_block,
+        vim.keymap.set({ 'n', 'v' }, '<Leader>mb', mb.line_breaks_toggle,
           { noremap = true, silent = true, desc = "Break/unbreak the paragraph/selection at the cursor" })
-        vim.keymap.set({ 'n', 'v' }, '<Leader>mq', mb.quote_block,
+        vim.keymap.set({ 'n', 'v' }, '<Leader>mq', mb.quotes_toggle,
           { noremap = true, silent = true, desc = "Quote/unquote paragraph/selection at the cursor" })
-        vim.keymap.set({ 'n', 'v' }, '<Leader>ml', mb.list_block,
+        vim.keymap.set({ 'n', 'v' }, '<Leader>ml', mb.bullet_list_toggle,
           { noremap = true, silent = true, desc = "Toggle list item bullets in paragraph/selection at the cursor" })
         vim.keymap.set({ 'n', 'v' }, '<Leader>mw', function()
           local col = vim.fn.input("Wrap at column number: ", tostring(wrap_column))
@@ -28,23 +28,24 @@ return {
         end, { noremap = true, silent = true, desc = "Wrap paragraph/selection" })
         vim.keymap.set({ 'n', 'v' }, '<Leader>mu', mb.unwrap_block,
           { noremap = true, silent = true, desc = "Unwrap paragraph/selection" })
-        vim.keymap.set({ 'n', 'v' }, '<Leader>mn', mb.number_block,
+        vim.keymap.set({ 'n', 'v' }, '<Leader>mn', mb.numbered_list_toggle,
           { silent = true, noremap = true, desc = "Toggle numbering non-indented lines" })
-        vim.keymap.set({ 'n', 'v' }, '<Leader>mr', function() mb.delimit_block('___', '___') end,
-          { silent = true, noremap = true, desc = "Surround paragraph/selection with rulers" })
-        vim.keymap.set({ 'n', 'v' }, '<Leader>mQ', function() mb.delimit_block('<blockquote>', '</blockquote>') end,
-          { silent = true, noremap = true, desc = "Surround paragraph/selection with HTML blockquote" })
-        vim.keymap.set({ 'n', 'v' }, '<Leader>mf', function() mb.delimit_block('```', '```') end,
-          { silent = true, noremap = true, desc = "Surround paragraph/selection with code fence" })
+        vim.keymap.set({ 'n', 'v' }, '<Leader>mr', function() mb.delimiters_toggle('___', '___') end,
+          { silent = true, noremap = true, desc = "Toggle ruler delimiters" })
+        vim.keymap.set({ 'n', 'v' }, '<Leader>mQ',
+          function() mb.delimiters_toggle('<blockquote>\n', '\n</blockquote>') end,
+          { silent = true, noremap = true, desc = "Toggle HTML blockquote delimiters" })
+        vim.keymap.set({ 'n', 'v' }, '<Leader>mf', function() mb.delimiters_toggle('```', '```') end,
+          { silent = true, noremap = true, desc = "Toggle code fence delimiters" })
         vim.keymap.set({ 'n', 'v' }, '<Leader>mF', function()
             local lang = vim.fn.input("Language: ")
-            mb.delimit_block('```' .. lang, '```')
+            mb.delimiters_toggle('```' .. lang, '```')
           end,
           {
             silent = true,
             noremap = true,
             desc =
-            "Surround paragraph/selection with code fence (prompt for language code)"
+            "Toggle code fence delimiters with language code"
           })
         vim.keymap.set({ 'n', 'v' }, '<Leader>mc', mb.csv_to_markdown_table,
           { silent = true, noremap = true, desc = "Convert CSV paragraph/selection to a Markdown table" })
