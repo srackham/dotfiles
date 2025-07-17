e3b2bdf8c6f20f4185d8f42c3dc103017af6eb81
Stuart Rackham <srackham@gmail.com>
2025-07-17 15:39:35 +1200

    zsh: bind M-r to fzf command history recall replacing C-r

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -303,9 +303,9 @@ vim.keymap.set({ 'i', 'n' }, '<M-t>', function() Utils.send_keys_to_terminal('Up
 vim.keymap.set('n', '<leader>tg', function() Utils.send_keys_to_terminal('lazygit Enter', { focus_pane_id = 2 }) end,
   { noremap = true, silent = true, desc = "Save and execute lazygit in the terminal pane" })
 
-vim.keymap.set('n', '<leader>tr', function() Utils.send_keys_to_terminal('C-r', { focus_pane_id = 2 }) end,
+vim.keymap.set('n', '<leader>tr', function() Utils.send_keys_to_terminal('M-r', { focus_pane_id = 2 }) end,
   { noremap = true, silent = true, desc = "Save and open command-line recall in the terminal pane" })
-vim.keymap.set({ 'i', 'n' }, '<M-r>', function() Utils.send_keys_to_terminal('C-r', { focus_pane_id = 2 }) end,
+vim.keymap.set({ 'i', 'n' }, '<M-r>', function() Utils.send_keys_to_terminal('M-r', { focus_pane_id = 2 }) end,
   { noremap = true, silent = true, desc = "Save and open command-line recall in the terminal pane" })
 
 -- Abbreviations commands
