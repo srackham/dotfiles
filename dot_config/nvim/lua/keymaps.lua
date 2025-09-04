4115befdf6fd79795054dd985589f4ffef1aaf44
Stuart Rackham <srackham@gmail.com>
2025-09-05 07:31:21 +1200

    nvim,zsh,tmux: reassign M-r to M-S-r and  C-r to M-r key bindings
    Because C-r clashed with the Vim register recall binding.

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -333,12 +333,12 @@ end, { noremap = true, silent = true, desc = "Toggle window word wrap" })
 
 vim.keymap.set('n', '<leader>tx', function() Utils.send_keys_to_terminal('Up Enter') end,
   { noremap = true, silent = true, desc = "Execute last terminal command (pane 2)" })
-vim.keymap.set({ 'i', 'n' }, '<M-r>', function() Utils.send_keys_to_terminal('Up Enter') end,
+vim.keymap.set({ 'i', 'n' }, '<M-S-r>', function() Utils.send_keys_to_terminal('Up Enter') end,
   { noremap = true, silent = true, desc = "Execute last terminal command (pane 2)" })
 
 vim.keymap.set('n', '<leader>tr', function() Utils.send_keys_to_terminal('C-r', { focus_pane_id = 2 }) end,
   { noremap = true, silent = true, desc = "Open fzf command-line recall in the terminal (pane 2)" })
-vim.keymap.set({ 'i', 'n' }, '<C-r>', function() Utils.send_keys_to_terminal('C-r', { focus_pane_id = 2 }) end,
+vim.keymap.set({ 'i', 'n' }, '<M-r>', function() Utils.send_keys_to_terminal('C-r', { focus_pane_id = 2 }) end,
   { noremap = true, silent = true, desc = "Open fzf command-line recall in the terminal (pane 2)" })
 
 -- Abbreviations commands
