5439e547e6f665466b1fce5b9a3072cbea8152df
Stuart Rackham <srackham@gmail.com>
2025-09-06 20:18:59 +1200

    nvim: strip trailing white space from files before saving

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -23,3 +23,11 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
     vim.keymap.set('n', '$', 'g$', { buffer = true })
   end
 })
+
+-- Strip trailing white space from files before saving
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.cmd([[%s/\s\+$//e]])
+  end,
+})
