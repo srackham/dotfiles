d4b495aea3c63187924565fcb825d36d6d49e2f8
Stuart Rackham <srackham@gmail.com>
2025-10-04 20:59:32 +1300

    nvim: expose nvim Lua dependencies to the Lua LSP server

--- dot_config/nvim/lsp/lua_ls.lua
+++ dot_config/nvim/lsp/lua_ls.lua
@@ -2,7 +2,10 @@ return {
   settings = {
     Lua = {
       diagnostics = {
-        globals = { "vim", "Obsidian" },
+        -- globals = { "vim", "Obsidian" },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
       },
     },
   },
