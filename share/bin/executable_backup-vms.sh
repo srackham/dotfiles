4d7b1ae526a4ffa5eeab1923c84054d9907c1227
Stuart Rackham <srackham@gmail.com>
2025-11-09 21:03:48 +1300

    backup-vms.sh: backup all share and public and treat symlinks as symlinks

--- share/bin/executable_backup-vms.sh
+++ share/bin/executable_backup-vms.sh
@@ -10,10 +10,10 @@ dst="/run/media/srackham/$DRIVE_LABEL"
 
 echo "Backing up data and VMs from '$src' to '$dst'"
 
-rclone sync --progress --modify-window=5s "$src/VirtualBox VMs" "$dst/VirtualBox VMs"
-rclone sync --progress --modify-window=5s "$src/.config/VirtualBox" "$dst/.config/VirtualBox"
-rclone sync --progress --modify-window=5s --include "/share/methods/prs/**" --include "/public/prs2k_methods/**" --include "/public/licmgr/**" "$src" "$dst"
+rclone sync --progress --links --modify-window=5s "$src/VirtualBox VMs" "$dst/VirtualBox VMs"
+rclone sync --progress --links --modify-window=5s "$src/.config/VirtualBox" "$dst/.config/VirtualBox"
+rclone sync --progress --links --modify-window=5s --include "/share/**" --include "/public/**" "$src" "$dst"
 
-rclone check --progress --modify-window=5s "$src/VirtualBox VMs" "$dst/VirtualBox VMs"
-rclone check --progress --modify-window=5s "$src/.config/VirtualBox" "$dst/.config/VirtualBox"
-rclone check --progress --modify-window=5s --include "/share/methods/prs/**" --include "/public/prs2k_methods/**" --include "/public/licmgr/**" "$src" "$dst"
+rclone check --progress --links --modify-window=5s "$src/VirtualBox VMs" "$dst/VirtualBox VMs"
+rclone check --progress --links --modify-window=5s "$src/.config/VirtualBox" "$dst/.config/VirtualBox"
+rclone check --progress --links --modify-window=5s --include "/share/**" --include "/public/**" "$src" "$dst"
