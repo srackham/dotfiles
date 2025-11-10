f703b953354bd2965bb36910c8934ab13dfb3f58
Stuart Rackham <srackham@gmail.com>
2025-11-10 13:40:35 +1300

    archive-vms.sh: treat symlinks as symlinks

--- share/bin/executable_archive-vms.sh
+++ share/bin/executable_archive-vms.sh
@@ -11,5 +11,5 @@ dst="/run/media/srackham/$DST_DRIVE_LABEL"
 
 echo "Archiving data and VMs from '$src' to '$dst'"
 
-rclone sync --progress --modify-window=5s "$src" "$dst"
-rclone check --progress --modify-window=5s "$src" "$dst"
+rclone sync --progress --links --modify-window=5s "$src" "$dst"
+rclone check --progress --links --modify-window=5s "$src" "$dst"
