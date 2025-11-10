04f7f6bb61cc0b96fb4184656ef9e7ab814b8afb
Stuart Rackham <srackham@gmail.com>
2025-11-10 14:20:14 +1300

    backup-vms.sh: use rsync instead of rclone for strict filesystem replication

--- share/bin/executable_backup-vms.sh
+++ share/bin/executable_backup-vms.sh
@@ -10,9 +10,15 @@ dst="/run/media/srackham/$DRIVE_LABEL"
 
 echo "Backing up data and VMs from '$src' to '$dst'"
 
-rclone sync --progress --links --modify-window=5s "$src/VirtualBox VMs" "$dst/VirtualBox VMs"
-rclone sync --progress --links --modify-window=5s "$src/.config/VirtualBox" "$dst/.config/VirtualBox"
-rclone sync --progress --links --modify-window=5s --include "/share/**" --include "/public/**" "$src" "$dst"
+# rclone sync --progress --links --modify-window=5s "$src/VirtualBox VMs" "$dst/VirtualBox VMs"
+# rclone sync --progress --links --modify-window=5s "$src/.config/VirtualBox" "$dst/.config/VirtualBox"
+# rclone sync --progress --links --modify-window=5s --include "/share/**" --include "/public/**" "$src" "$dst"
+
+# rsync is preferable to rclone for strict replication of permissions and ownership.
+# --inplace overwrites destination files directly instead of creating a temporary file and renaming whic benefits SSD drives.
+rsync -av --delete --inplace "$src/VirtualBox VMs/" "$dst/VirtualBox VMs"
+rsync -av --delete --inplace "$src/.config/VirtualBox/" "$dst/.config/VirtualBox"
+rsync -av --delete --inplace --include "/share/*** " --include "/public/*** " --exclude "/*" "$src/" "$dst"
 
 rclone check --progress --links --modify-window=5s "$src/VirtualBox VMs" "$dst/VirtualBox VMs"
 rclone check --progress --links --modify-window=5s "$src/.config/VirtualBox" "$dst/.config/VirtualBox"
