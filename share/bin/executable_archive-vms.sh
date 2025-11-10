6f53e0dbc259a8c4bda528502e8d3ae824e028b1
Stuart Rackham <srackham@gmail.com>
2025-11-10 14:53:26 +1300

    backup-vms.sh: squash the 3 rsync commands into a single command

--- share/bin/executable_archive-vms.sh
+++ share/bin/executable_archive-vms.sh
@@ -11,5 +11,6 @@ dst="/run/media/srackham/$DST_DRIVE_LABEL"
 
 echo "Archiving data and VMs from '$src' to '$dst'"
 
+# rclone used instead of rsync to allow the destination archive to be an NTFS USB drive.
 rclone sync --progress --links --modify-window=5s "$src" "$dst"
 rclone check --progress --links --modify-window=5s "$src" "$dst"
--- share/bin/executable_backup-vms.sh
+++ share/bin/executable_backup-vms.sh
@@ -10,16 +10,8 @@ dst="/run/media/srackham/$DRIVE_LABEL"
 
 echo "Backing up data and VMs from '$src' to '$dst'"
 
-# rclone sync --progress --links --modify-window=5s "$src/VirtualBox VMs" "$dst/VirtualBox VMs"
-# rclone sync --progress --links --modify-window=5s "$src/.config/VirtualBox" "$dst/.config/VirtualBox"
-# rclone sync --progress --links --modify-window=5s --include "/share/**" --include "/public/**" "$src" "$dst"
-
 # rsync is preferable to rclone for strict replication of permissions and ownership.
 # --inplace overwrites destination files directly instead of creating a temporary file and renaming whic benefits SSD drives.
-rsync -av --delete --inplace "$src/VirtualBox VMs/" "$dst/VirtualBox VMs"
-rsync -av --delete --inplace "$src/.config/VirtualBox/" "$dst/.config/VirtualBox"
-rsync -av --delete --inplace --include "/share/*** " --include "/public/*** " --exclude "/*" "$src/" "$dst"
+rsync -av --delete --inplace --include "/VirtualBox VMs/***" --include "/.config/***" --include "/share/***" --include "/public/***" --exclude "/*" "$src/" "$dst"
 
-rclone check --progress --links --modify-window=5s "$src/VirtualBox VMs" "$dst/VirtualBox VMs"
-rclone check --progress --links --modify-window=5s "$src/.config/VirtualBox" "$dst/.config/VirtualBox"
-rclone check --progress --links --modify-window=5s --include "/share/**" --include "/public/**" "$src" "$dst"
+rclone check --progress --links --modify-window=5s --include "/VirtualBox VMs/**" --include "/.config/**" --include "/share/**" --include "/public/**" "$src" "$dst"
