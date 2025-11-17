4ccaf2b9fa116f438204adb4f24a9078918a5b17
Stuart Rackham <srackham@gmail.com>
2025-11-18 09:47:10 +1300

    nvim: doc comment

--- dot_config/nvim/lsp/rust_analyzer.lua
+++ dot_config/nvim/lsp/rust_analyzer.lua
@@ -2,7 +2,7 @@ return {
   settings = {
     filetypes = { "rust" },
     ["rust-analyzer"] = {
-      -- Add any rust-analyzer custom settings if desired
+      -- Add any rust-analyzer custom settings (https://github.com/rust-lang/rust-analyzer/blob/master/crates/rust-analyzer/src/config.rs)
     },
   },
 }
