12b9a40d566ee0504daf45d06504c2eb7dc524a5
Stuart Rackham <srackham@gmail.com>
2025-12-03 16:16:37 +1300

    wezterm: add tab line plugin

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -64,67 +64,91 @@ config.keys = {
 
 }
 
--- Tab bar
-local tab_color = "#84a2bd" -- Active tab and status text color
+wezterm.on('augment-command-palette', function(_, _)
+  return {
+    {
+      brief = 'Rename tab',
+      icon = 'md_rename_box',
+      action = act.PromptInputLine {
+        description = 'Enter new name for tab',
+        action = wezterm.action_callback(function(window, _, line)
+          if line then
+            window:active_tab():set_title(line)
+          end
+        end),
+      },
+    },
+  }
+end)
 
+-- Tab bar
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = true
 
-wezterm.on('update-right-status', function(window, _)
-  local date = wezterm.strftime '%H:%M  %a %d %b %Y'
-  local hostname = wezterm.hostname()
-  window:set_right_status(wezterm.format {
-    { Attribute = { Italic = true } },
-    { Attribute = { Intensity = "Bold" } },
-    { Foreground = { Color = tab_color } },
-    { Text = hostname .. ' | ' .. date },
-  })
-end)
+--  Tab bar plugin
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+tabline.setup()
 
+-- TODO: Drop old tab bar configuration.
+-- local tab_color = "#84a2bd" -- Active tab and status text color
+
+-- wezterm.on('update-right-status', function(window, _)
+--   local date = wezterm.strftime '%H:%M  %a %d %b %Y'
+--   local hostname = wezterm.hostname()
+--   window:set_right_status(wezterm.format {
+--     { Attribute = { Italic = true } },
+--     { Attribute = { Intensity = "Bold" } },
+--     { Foreground = { Color = tab_color } },
+--     { Text = hostname .. ' | ' .. date },
+--   })
+-- end)
+
+-- Color overrides
 config.colors = {
   background = 'black',
   split = '#585858', -- The color of the split lines between panes
-  tab_bar = {
-    background = "#262626",
-
-    active_tab = {
-      bg_color = tab_color,
-      fg_color = "black",
-      intensity = "Bold",
-    },
-
-    inactive_tab = {
-      bg_color = "#444444",
-      fg_color = "#c0c0c0",
-    },
-
-    inactive_tab_hover = {
-      bg_color = "#4e4e4e",
-      fg_color = "#ffffff",
-    },
-
-    new_tab = {
-      bg_color = "#3a3a3a",
-      fg_color = "#c0c0c0",
-    },
-
-    new_tab_hover = {
-      bg_color = "#444444",
-      fg_color = "#ffffff",
-      italic = true,
-      intensity = "Bold",
-    },
-  },
+  -- TODO: Drop old tab bar configuration.
+  -- tab_bar = {
+  --   background = "#262626",
+  --
+  --   active_tab = {
+  --     bg_color = tab_color,
+  --     fg_color = "black",
+  --     intensity = "Bold",
+  --   },
+  --
+  --   inactive_tab = {
+  --     bg_color = "#444444",
+  --     fg_color = "#c0c0c0",
+  --   },
+  --
+  --   inactive_tab_hover = {
+  --     bg_color = "#4e4e4e",
+  --     fg_color = "#ffffff",
+  --   },
+  --
+  --   new_tab = {
+  --     bg_color = "#3a3a3a",
+  --     fg_color = "#c0c0c0",
+  --   },
+  --
+  --   new_tab_hover = {
+  --     bg_color = "#444444",
+  --     fg_color = "#ffffff",
+  --     italic = true,
+  --     intensity = "Bold",
+  --   },
+  -- },
 }
 
 -- Window border
 config.window_frame = {
-  border_left_width = '3px',
-  border_right_width = '3px',
-  border_bottom_height = '3px',
-  border_top_height = '3px',
+  border_left_width = '2px',
+  border_right_width = '2px',
+  border_bottom_height = '2px',
+  border_top_height = '2px',
   border_left_color = '#3a3a3a',
   border_right_color = '#3a3a3a',
   border_bottom_color = '#3a3a3a',
