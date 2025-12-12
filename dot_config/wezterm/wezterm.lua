d72b94a5ad20d65af3b0603c55581e4e1129240e
Stuart Rackham <srackham@gmail.com>
2025-12-12 15:13:37 +1300

    wezterm: dropped resurrect.wezterm plugin

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -196,7 +196,7 @@ tabline.setup({
   },
 })
 
--- Rename tab command
+-- Rename tab Palette command
 table.insert(palette_commands,
   {
     brief = 'Rename tab',
@@ -212,87 +212,19 @@ table.insert(palette_commands,
   }
 )
 
--- Layout save/restore plugin
-local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
-
--- Resurrect on startup FIXME: this startup code does not work.
--- wezterm.on("gui-startup", resurrect.state_manager.resurrect_on_gui_startup)
-
-local function update_plugins_action(window)
-  wezterm.log_info("Updating plugins...")
-  wezterm.plugin.update_all()
-  wezterm.reload_configuration()
-  window:toast_notification("WezTerm", "Plugins updated", nil, 4000)
-end
-
-local function save_workspace_state_action(window)
-  resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
-  window:toast_notification("WezTerm", "Workspace state saved", nil, 4000)
-end
-
-local function load_workspace_state_action(win, pane)
-  resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id)
-    local type = string.match(id, "^([^/]+)") -- match before '/'
-    id = string.match(id, "([^/]+)$")         -- match after '/'
-    id = string.match(id, "(.+)%..+$")        -- remove file extension
-    local opts = {
-      relative = true,
-      restore_text = true,
-      on_pane_restore = resurrect.tab_state.default_on_pane_restore,
-    }
-    if type == "workspace" then
-      local state = resurrect.state_manager.load_state(id, "workspace")
-      resurrect.workspace_state.restore_workspace(state, opts)
-    elseif type == "window" then
-      local state = resurrect.state_manager.load_state(id, "window")
-      resurrect.window_state.restore_window(pane:window(), state, opts)
-    elseif type == "tab" then
-      local state = resurrect.state_manager.load_state(id, "tab")
-      resurrect.tab_state.restore_tab(pane:tab(), state, opts)
-    end
-  end)
-end
-
+-- Update plugins Palette command
 table.insert(palette_commands,
   {
     brief = 'Update Plugins',
     icon = 'md_rename_box',
-    action = wezterm.action_callback(update_plugins_action),
-  }
-)
-
-table.insert(palette_commands,
-  {
-    brief = 'Save workspace state',
-    icon = 'md_content_save',
-    action = wezterm.action_callback(save_workspace_state_action),
-  }
-)
-
-table.insert(palette_commands,
-  {
-    brief = 'Load workspace state',
-    icon = 'md_reload',
-    action = wezterm.action_callback(load_workspace_state_action),
-  }
-)
-
--- Delete workspace state command
-table.insert(palette_commands,
-  {
-    brief = 'Delete workspace state',
-    icon = 'md_delete',
-    action = wezterm.action_callback(function(win, pane)
-      resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id)
-          resurrect.state_manager.delete_state(id)
-        end,
-        {
-          title = "Delete State",
-          description = "Select State to Delete and press Enter = accept, Esc = cancel, / = filter",
-          fuzzy_description = "Search State to Delete: ",
-          is_fuzzy = true,
-        })
-    end),
+    action = wezterm.action_callback(
+      function(window)
+        wezterm.log_info("Updating plugins...")
+        wezterm.plugin.update_all()
+        wezterm.reload_configuration()
+        window:toast_notification("WezTerm", "Plugins updated", nil, 4000)
+      end
+    ),
   }
 )
 
