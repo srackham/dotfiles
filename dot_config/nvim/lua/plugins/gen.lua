95ee1356332ccebae52e5c9f6ce779bb971d03b7
Stuart Rackham <srackham@gmail.com>
2025-12-29 15:48:16 +1300

    nvim: add gen.nvim plugin

---
+++ dot_config/nvim/lua/plugins/gen.lua
@@ -0,0 +1,8 @@
+return {
+  "David-Kunz/gen.nvim",
+  enabled = true,
+  opts = {
+    model = "qwen3-coder:480b-cloud", -- The default model to use.
+    display_mode = "horizontal-split", -- The display mode. Can be "float" or "split" or "horizontal-split" or "vertical-split".
+  },
+}
