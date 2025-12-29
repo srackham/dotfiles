2ca220090a6d2c060888ec106ef2492b7aa00ed6
Stuart Rackham <srackham@gmail.com>
2025-12-30 07:52:32 +1300

    nvim(gen.nvim): add custom prompts

--- dot_config/nvim/lua/plugins/gen.lua
+++ dot_config/nvim/lua/plugins/gen.lua
@@ -1,23 +1,30 @@
 return {
   "David-Kunz/gen.nvim",
   enabled = true,
-  opts = {
-    model = "qwen3-coder:480b-cloud", -- The default model to use.
-    display_mode = "horizontal-split", -- The display mode. Can be "float" or "split" or "horizontal-split" or "vertical-split".
-    show_prompt = true, -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
-    no_auto_close = true, -- Never closes the window automatically.
-  },
-  keys = {
-    { "<leader>lg", ":Gen<CR>", mode = { "n", "v" }, desc = "Gen.nvim generate" },
-    { "<leader>lc", ":Gen Chat<CR>", mode = { "n", "v" }, desc = "Gen.nvim chat" },
-    { "<leader>ls", ":Gen Enhance_Grammar_Spelling<CR>", mode = { "n", "v" }, desc = "Gen.nvim spelling and grammar" },
-    {
-      "<leader>lm",
-      function()
-        require("gen").select_model()
-      end,
-      mode = { "n", "v" },
-      desc = "Gen.nvim model",
-    },
-  },
+  config = function()
+
+    local gen = require "gen"
+
+    -- Non-default options
+    gen.setup {
+      model = "qwen3-coder:480b-cloud", -- The default model to use.
+      display_mode = "horizontal-split", -- The display mode. Can be "float" or "split" or "horizontal-split" or "vertical-split".
+      show_prompt = true, -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
+    }
+
+    -- Custom prompts
+    gen.prompts["Enhance_Grammar_Spelling"].replace = false -- Don't automatically replace
+    gen.prompts["Fix_Spelling"] = {
+      prompt = "Fix spelling errors in the following text, just output the final text without additional quotes around it:\n$text",
+      replace = true,
+    }
+
+    -- Custom key mappings
+    vim.keymap.set({ "n", "v" }, "<leader>lg", ":Gen<CR>", { desc = "Gen.nvim generate" })
+    vim.keymap.set({ "n", "v" }, "<leader>lc", ":Gen Chat<CR>", { desc = "Gen.nvim chat" })
+    vim.keymap.set({ "n", "v" }, "<leader>ls", ":Gen Enhance_Grammar_Spelling<CR>", { desc = "Gen.nvim spelling and grammar" })
+    vim.keymap.set({ "n", "v" }, "<leader>lm", function()
+      gen.select_model()
+    end, { desc = "Gen.nvim model" })
+  end,
 }
