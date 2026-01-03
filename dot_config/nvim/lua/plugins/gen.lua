80cff4623bb749c2d1dc449835b684a2a158bbc1
Stuart Rackham <srackham@gmail.com>
2026-01-04 09:04:34 +1300

    nvim(gen.nvim): update key mappings

--- dot_config/nvim/lua/plugins/gen.lua
+++ dot_config/nvim/lua/plugins/gen.lua
@@ -15,21 +15,51 @@ return {
     }
 
     -- Custom prompts
-    gen.prompts["Enhance_Grammar_Spelling"].replace = false -- Don't automatically replace
-    gen.prompts["Fix_Spelling"] = {
-      prompt = "Fix spelling errors in the following text, just output the final text without additional quotes around it:\n$text",
+    local function delete_prompt(name)
+      gen.prompts[name] = nil
+    end
+
+    local function rename_prompt(from, to)
+      gen.prompts[to] = gen.prompts[from]
+      delete_prompt(from)
+    end
+
+    gen.prompts["Spelling"] = {
+      prompt = "Correct spelling and minor grammar mistakes in the given text. Do not change the meaning, style or formatting.\n\n$text",
       replace = true,
     }
     gen.prompts["Explain_Code"] = {
-      prompt = "Explain the following code:\n```$filetype\n$text\n```",
+      prompt = "Explain the following $filetype code:\n\n```\n$text\n```",
     }
+    gen.prompts["Meaning"] = {
+      prompt = "Explain the meaning of the following text:\n$text",
+    }
+    gen.prompts["Synonyms"] = {
+      prompt = "List synonyms for the following word: $text",
+    }
+    gen.prompts["Latin_to_English"] = {
+      prompt = "Translate the following Latin text to English:\n\n$text",
+    }
+    rename_prompt("Enhance_Grammar_Spelling", "Grammar_and_Spelling")
 
     -- Custom key mappings
-    vim.keymap.set({ "n", "v" }, "<leader>lp", ":Gen<CR>", { desc = "Gen.nvim command palette" })
+    local function execute_prompt(prompt)
+      -- If in normal mode select the word at the cursor.
+      local mode = vim.api.nvim_get_mode().mode
+      if mode == "n" then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("viw:Gen " .. prompt .. "<CR>", true, false, true), "n", false)
+      else
+        vim.cmd("Gen " .. prompt)
+      end
+    end
+
+    vim.keymap.set({ "n", "v" }, "<leader>ll", ":Gen<CR>", { desc = "Gen.nvim command palette" })
+    vim.keymap.set({ "n", "v" }, "<M-\\>", ":Gen<CR>", { desc = "Gen.nvim command palette" })
     vim.keymap.set({ "n", "v" }, "<leader>lc", ":Gen Chat<CR>", { desc = "Gen.nvim chat" })
-    vim.keymap.set({ "n", "v" }, "<leader>ls", ":Gen Enhance_Grammar_Spelling<CR>", { desc = "Gen.nvim spelling and grammar" })
-    vim.keymap.set({ "n", "v" }, "<leader>lm", function()
-      gen.select_model()
-    end, { desc = "Gen.nvim model" })
+    vim.keymap.set({ "n", "v" }, "<leader>lm", gen.select_model, { desc = "Gen.nvim model" })
+    vim.keymap.set({ "n", "v" }, "<leader>ls", ":Gen Spelling<CR>", { desc = "Gen.nvim spelling correction" })
+    vim.keymap.set({ "n", "v" }, "<leader>lS", function()
+      execute_prompt "Synonyms"
+    end, { desc = "Gen.nvim list synonyms" })
   end,
 }
