45ee309522842ba98f82c00795bb9225c2af5b47
Stuart Rackham <srackham@gmail.com>
2026-01-25 21:31:26 +1300

    nvim(gen.nvim): new and updated key mappings

--- dot_config/nvim/lua/plugins/gen.lua
+++ dot_config/nvim/lua/plugins/gen.lua
@@ -30,36 +30,42 @@ return {
     vim.keymap.set({ "n", "v" }, "<leader>ls", ":Gen Synonyms<cr>", { desc = "Gen.nvim list synonyms" })
     vim.keymap.set({ "n", "v" }, "<leader>ld", ":Gen Dictionary<CR>", { desc = "Gen.nvim dictionary lookup" })
     vim.keymap.set({ "n", "v" }, "<leader>l.", ":Gen .<CR>", { desc = "Gen.nvim repeat previous command" })
-    vim.keymap.set({ "n", "v" }, "<leader>lm", gen.select_model, { desc = "Gen.nvim model selection" })
-    vim.keymap.set({ "n", "v" }, "<leader>lo", "<Cmd>Gen /open<CR>", { desc = "Gen.nvim open the response window" })
     vim.keymap.set({ "n", "v" }, "<leader>lp", "<Cmd>Gen /prompts<CR>", { desc = "Gen.nvim prompts file manager" })
+    vim.keymap.set({ "n", "v" }, "<leader>lm", "<Cmd>Gen /models<CR>", { desc = "Gen.nvim model selection" })
+    vim.keymap.set({ "n", "v" }, "<M-\\>", "<Cmd>Gen /responses<CR>", { desc = "Gen.nvim toggle response window" })
     vim.keymap.set(
       { "n", "v" },
       "<leader>lr",
       "<Cmd>Gen /reset<CR>",
       { desc = "Gen.nvim reset the model context and clear the response window" }
     )
-    vim.keymap.set({ "n", "v" }, "<M-\\>", "<Cmd>Gen /toggle<CR>", { desc = "Gen.nvim toggle response window" })
-    vim.keymap.set({ "n" }, "<leader>ll", function()
+    vim.keymap.set({ "n", "v" }, "<leader>ll", function()
       vim.cmd("edit " .. gen.log_file(gen))
     end, { desc = "Gen.nvim open current log file" })
-    vim.keymap.set({ "n", "v" }, "<Leader>lf", function()
+    vim.keymap.set({ "n", "v" }, "<Leader>lF", function()
       require("telescope.builtin").find_files {
         cwd = gen.logs_dir,
         prompt_title = "Find log files",
       }
     end, { desc = "Gen.nvim find log files" })
-    vim.keymap.set({ "n", "v" }, "<Leader>lg", function()
-      require("telescope.builtin").live_grep {
-        cwd = gen.prompts_dir,
-        prompt_title = "Live Grep prompts files",
-      }
-    end, { desc = "Gen.nvim live-grep prompts files" })
     vim.keymap.set({ "n", "v" }, "<Leader>lG", function()
       require("telescope.builtin").live_grep {
         cwd = gen.logs_dir,
         prompt_title = "Live Grep log files",
       }
     end, { desc = "Gen.nvim live-grep log files" })
+
+    vim.keymap.set({ "n", "v" }, "<Leader>lf", function()
+      require("telescope.builtin").find_files {
+        cwd = gen.prompts_dir,
+        prompt_title = "Find prompts files",
+      }
+    end, { desc = "Gen.nvim find prompts files" })
+    vim.keymap.set({ "n", "v" }, "<Leader>lg", function()
+      require("telescope.builtin").live_grep {
+        cwd = gen.prompts_dir,
+        prompt_title = "Live Grep prompts files",
+      }
+    end, { desc = "Gen.nvim live-grep prompts files" })
   end,
 }
