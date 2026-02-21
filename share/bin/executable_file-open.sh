c59f7968c7404329d72cf612f56d898a31b34739
Stuart Rackham <srackham@gmail.com>
2026-02-21 14:53:37 +1300

    file-open.sh: fix so it can open file in Neovim in the first WezTerm pane

--- share/bin/executable_file-open.sh
+++ share/bin/executable_file-open.sh
@@ -6,8 +6,8 @@
 #
 
 if [ $# -eq 0 ]; then
-	echo "Usage: $(basename $0) <file-name>"
-	exit 1
+    echo "Usage: $(basename "$0") <file-name>"
+    exit 1
 fi
 
 file_path=$(realpath "$1")
@@ -16,16 +16,28 @@ file_path=$(realpath "$1")
 mime_type=$(file --mime-type -b "$file_path")
 
 if [[ "$mime_type" != text/* ]]; then
-	# Not a plain text file, open in web browser
-	if command -v xdg-open >/dev/null 2>&1; then
-		xdg-open "$file_path"
-	else
-		echo "No suitable command found to open the file in a browser."
-		exit 1
-	fi
+    # Not a plain text file, open in web browser
+    if command -v xdg-open >/dev/null 2>&1; then
+        xdg-open "$file_path"
+    else
+        echo "No suitable command found to open the file in a browser."
+        exit 1
+    fi
 else
-	# Plain text file, open in tmux Neovim pane 1
-	tmux send-keys -t:.1 Escape &&
-		tmux send-keys -t:.1 ":edit $file_path" C-m &&
-		tmux select-pane -Z -t:.1
+    # # Plain text file, open in tmux Neovim pane 1
+    # tmux send-keys -t:.1 Escape &&
+    # 	tmux send-keys -t:.1 ":edit $file_path" C-m &&
+    # 	tmux select-pane -Z -t:.1
+
+    # Plain text file, open in the first pane in the current WezTerm tab (assumed to be running Neovim)
+    PANE_ID=$(
+        wezterm cli list --format json |
+            jq -r --arg cur "$WEZTERM_PANE" '
+      (map(select(.pane_id == ($cur|tonumber)))[0].tab_id) as $tab
+      | map(select(.tab_id == $tab))
+      | sort_by(.pane_index)
+      | .[0].pane_id
+    '
+    )
+    wezterm cli send-text --pane-id "$PANE_ID" --no-paste ":edit $file_path"$'\r'
 fi
