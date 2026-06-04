872a4c12476edbebe465ce90f46b93e659b9af1f
Stuart Rackham <srackham@gmail.com>
2026-06-04 19:56:00 +1200

    nvim: whisper.nvim: add small.en model and Lualine support

--- dot_config/nvim/lua/plugins/whisper.lua
+++ dot_config/nvim/lua/plugins/whisper.lua
@@ -3,9 +3,22 @@ return {
   config = function()
     require("whisper").setup {
       auto_download_model = false,
-      model = "base.en",
+      model = "small.en",
       keybind = "<C-g>",
       manual_trigger_key = "<Space>",
+      step_ms = 3000,
+      length_ms = 5000,
+    }
+    -- Lualine integration
+    require("lualine").setup {
+      sections = {
+        lualine_x = {
+          require("whisper").lualine_component,
+          "encoding",
+          "fileformat",
+          "filetype",
+        },
+      },
     }
   end,
   keys = {
