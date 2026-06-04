f0ecbb91671050dc15cdaf0c42718aae3ba0e0cc
Stuart Rackham <srackham@gmail.com>
2026-06-05 08:07:24 +1200

    nvim: whisper.nvim: disabled

--- dot_config/nvim/lua/plugins/whisper.lua
+++ dot_config/nvim/lua/plugins/whisper.lua
@@ -1,11 +1,15 @@
+local toggle_key = "<C-g>"
+local trigger_key = "<Space>"
+
 return {
   "Avi-D-coder/whisper.nvim",
+  enabled = false,
   config = function()
     require("whisper").setup {
       auto_download_model = false,
       model = "small.en",
-      keybind = "<C-g>",
-      manual_trigger_key = "<Space>",
+      keybind = toggle_key,
+      manual_trigger_key = trigger_key,
       step_ms = 3000,
       length_ms = 5000,
     }
@@ -22,6 +26,6 @@ return {
     }
   end,
   keys = {
-    { "<C-g>", mode = { "n", "i", "v" }, desc = "Load the plugin" },
+    { toggle_key, mode = { "n", "i", "v" }, desc = "Load the plugin" },
   },
 }
