6b969b6b2e4d695ec713b9a5c8106bcfc888b91b
Stuart Rackham <srackham@gmail.com>
2026-06-16 08:29:58 +1200

    nvim: disable unused plugins

---
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "antoinemadec/FixCursorHold.nvim", enabled = false },
+  { "nvim-neotest/neotest", enabled = false },
+  { "markemmons/neotest-deno", enabled = false },
+  { "tpope/vim-abolish", enabled = false },
+}
