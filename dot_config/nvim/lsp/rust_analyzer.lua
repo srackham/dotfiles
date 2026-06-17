4c09ea1e9f5e053b16e6fc2ea23bd2f0e5f1adac
Stuart Rackham <srackham@gmail.com>
2026-06-17 19:38:27 +1200

    nvim: upgrade to nvim 0.12 LSP configuration
    See /home/srackham/share/notes/neovim-011-to-012-migration.md

--- dot_config/nvim/lsp/rust_analyzer.lua
+++ dot_config/nvim/lsp/rust_analyzer.lua
@@ -1,6 +1,6 @@
 return {
+  filetypes = { "rust" },
   settings = {
-    filetypes = { "rust" },
     ["rust-analyzer"] = {
       -- Add any rust-analyzer custom settings (https://github.com/rust-lang/rust-analyzer/blob/master/crates/rust-analyzer/src/config.rs)
     },
--- dot_config/nvim/lua/lsp_init.lua
+++ dot_config/nvim/lua/lsp_init.lua
@@ -1,5 +1,5 @@
 -- Load LSP config files in ~/.config/nvim/lsp/ then activate the LSP servers.
-vim.lsp.enable { "lua_ls", "tsserver", "denols", "rust_analyzer", "bashls", "marksman", "gleam", "nixd", "ruff" }
+vim.lsp.enable { "lua_ls", "ts_ls", "denols", "rust_analyzer", "bashls", "marksman", "gleam", "nixd", "ruff" }
 
 -- Shared diagnostic config
 vim.diagnostic.config {
