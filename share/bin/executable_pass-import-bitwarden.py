0bd31aa9bf9fd7b5abba3ec04d10d381e138f67e
Stuart Rackham <srackham@gmail.com>
2026-06-21 09:26:24 +1200

    pass-import-bitwarden: drop 'bitwarden/' name prefix; add website field

--- share/bin/executable_pass-import-bitwarden.py
+++ share/bin/executable_pass-import-bitwarden.py
@@ -19,14 +19,17 @@ def main():
             name = row["name"]
             username = row["login_username"]
             password = row["login_password"]
+            website = row["login_uri"]
             notes = row.get("notes", "")
 
             if row.get("type") == "note":
                 content = f"{notes}\n"
-                pass_name = f"bitwarden/notes/{name}"
+                pass_name = f"notes/{name}"
             else:
-                content = f"{password}\nlogin: {username}\n\n{notes}\n"
-                pass_name = f"bitwarden/logins/{name}"
+                content = (
+                    f"{password}\nlogin: {username}\nwebsite: {website}\n\n{notes}\n"
+                )
+                pass_name = f"logins/{name}"
 
             subprocess.run(
                 ["pass", "insert", "--force", "--multiline", pass_name],
