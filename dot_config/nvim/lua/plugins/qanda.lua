590beeed73126c2bde8777b52697d69515c7c3c5
Stuart Rackham <srackham@gmail.com>
2026-07-06 08:05:57 +1200

    update template picker command names; tab opens chat window

--- dot_config/nvim/lua/plugins/qanda.lua
+++ dot_config/nvim/lua/plugins/qanda.lua
@@ -13,7 +13,7 @@ return {
     qanda.setup {
       data_dir = "~/share/data/qanda_nvim",
       user_prompt_lines = 5,
-      system_prompt_lines = 5,
+      system_message_lines = 5,
       provider_options = {
         ollama = { temperature = 0.4 },
       },
@@ -22,14 +22,24 @@ return {
     }
 
     -- Key mappings for builtin commands --
-    vim.keymap.set("n", "<Tab>", "<Cmd>Qanda /prompt_window<CR>", { desc = "Qanda.nvim open user prompt window" })
+    vim.keymap.set("n", "<Tab>", "<Cmd>Qanda /chat_window<CR>", { desc = "Qanda.nvim open user chat window" })
     vim.keymap.set({ "n", "v" }, "<Leader>lq", "<Cmd>Qanda /prompt_window<CR>", { desc = "Qanda.nvim open Prompt window" })
     vim.keymap.set({ "n", "v", "i" }, "<C-Del>", "<Cmd>Qanda /new_prompt<CR>", { desc = "Qanda.nvim open new prompt" })
-    vim.keymap.set({ "n", "v" }, "<Leader>lp", "<Cmd>Qanda /prompt_picker<CR>", { desc = "Qanda.nvim open prompts picker" })
+    vim.keymap.set(
+      { "n", "v" },
+      "<Leader>lp",
+      "<Cmd>Qanda /prompt_template_picker<CR>",
+      { desc = "Qanda.nvim open prompts template picker" }
+    )
     vim.keymap.set({ "n", "v" }, "<Leader>la", "<Cmd>Qanda /chat_window<CR>", { desc = "Qanda.nvim open Chat window" })
     vim.keymap.set({ "n", "v" }, "<Leader>lc", "<Cmd>Qanda /chat_picker<CR>", { desc = "Qanda.nvim open Chat picker" })
     vim.keymap.set({ "n", "v" }, "<Leader>ln", "<Cmd>Qanda /new_chat<CR>", { desc = "Qanda.nvim new chat" })
-    vim.keymap.set({ "n", "v" }, "<Leader>ls", "<Cmd>Qanda /system_message_picker<CR>", { desc = "Qanda.nvim open System Messages picker" })
+    vim.keymap.set(
+      { "n", "v" },
+      "<Leader>ls",
+      "<Cmd>Qanda /system_template_picker<CR>",
+      { desc = "Qanda.nvim open System template picker" }
+    )
     vim.keymap.set({ "n", "v" }, "<leader>lm", "<Cmd>Qanda /model_picker<CR>", { desc = "Qanda.nvim model selection" })
     vim.keymap.set({ "n", "v" }, "<leader>lP", "<Cmd>Qanda /provider_picker<CR>", { desc = "Qanda.nvim provider selection" })
     vim.keymap.set({ "n", "v" }, "<leader>lr", "<Cmd>Qanda /recent_models<CR>", { desc = "Qanda.nvim recent model selection" })
