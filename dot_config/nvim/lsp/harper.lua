c224d446ea8f7fd157667b573c67ce3a92b1e12f
Stuart Rackham <srackham@gmail.com>
2026-07-23 18:32:53 +1200

    nvim: harper: change rule names from snake_case to PascalCase

--- dot_config/nvim/lsp/harper.lua
+++ dot_config/nvim/lsp/harper.lua
@@ -2,7 +2,6 @@ return {
   cmd = { "harper-ls", "--stdio" },
   root_markers = { ".git" },
 
-  -- 1. Add programming languages to allow comment checking
   filetypes = {
     "markdown",
     "text",
@@ -20,17 +19,14 @@ return {
 
   settings = {
     ["harper-ls"] = {
-      userDictPath = "~/.config/harper/dict.txt",
-
-      -- 2. Downgrade severity to "hint" or "information" so warnings don't clutter your editor
       diagnosticSeverity = "hint",
 
-      -- 3. Turn off overly strict style/grammar rules that cause noise
+      -- Turn off overly strict style/grammar rules that cause noise
       linters = {
-        spell_check = true,
+        spell_check = false,
         spelled_numbers = false, -- Stop complaining about "3" vs "three"
-        long_sentences = false, -- Turning this off drastically reduces noise
-        sentence_capitalization = true,
+        long_sentences = false,
+        sentence_capitalization = false,
         repeated_words = true,
         spaces = true,
       },
