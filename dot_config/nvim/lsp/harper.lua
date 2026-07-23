a5c8011ecb909ec25bc43744120d02e39ed63e6f
Stuart Rackham <srackham@gmail.com>
2026-07-23 15:38:45 +1200

    nvim: make harper less noisy

--- dot_config/nvim/lsp/harper.lua
+++ dot_config/nvim/lsp/harper.lua
@@ -1,5 +1,39 @@
 return {
   cmd = { "harper-ls", "--stdio" },
-  filetypes = { "markdown", "text", "typst", "tex" },
   root_markers = { ".git" },
+
+  -- 1. Add programming languages to allow comment checking
+  filetypes = {
+    "markdown",
+    "text",
+    "typst",
+    "tex",
+    "lua",
+    "python",
+    "rust",
+    "go",
+    "javascript",
+    "typescript",
+    "c",
+    "cpp",
+  },
+
+  settings = {
+    ["harper-ls"] = {
+      userDictPath = "~/.config/harper/dict.txt",
+
+      -- 2. Downgrade severity to "hint" or "information" so warnings don't clutter your editor
+      diagnosticSeverity = "hint",
+
+      -- 3. Turn off overly strict style/grammar rules that cause noise
+      linters = {
+        spell_check = true,
+        spelled_numbers = false, -- Stop complaining about "3" vs "three"
+        long_sentences = false, -- Turning this off drastically reduces noise
+        sentence_capitalization = true,
+        repeated_words = true,
+        spaces = true,
+      },
+    },
+  },
 }
