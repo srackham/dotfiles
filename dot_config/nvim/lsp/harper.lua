388a524b532400e2b83b081f9f3c3355f247aab4
Stuart Rackham <srackham@gmail.com>
2026-07-24 18:22:41 +1200

    nvim: harper: disable/enable the harper LSP server (instead of just hiding the diagnostics text)

--- dot_config/nvim/lsp/harper.lua
+++ dot_config/nvim/lsp/harper.lua
@@ -1,20 +1,22 @@
-
-local virtual_text_enabled = true
+local harper_enabled = true -- track whether harper is enabled
 
 vim.keymap.set("n", "<Leader>dh", function()
   local clients = vim.lsp.get_clients { name = "harper" }
-  if #clients == 0 then
-    vim.notify("harper-ls not attached", vim.log.levels.WARN)
-    return
+
+  if harper_enabled then
+    -- Disable: stop all harper clients
+    for _, client in ipairs(clients) do
+      client:stop()
+    end
+    harper_enabled = false
+    vim.notify "Harper LSP server disabled"
+  else
+    -- Enable: re-attach harper to current and future buffers
+    vim.lsp.enable "harper"
+    harper_enabled = true
+    vim.notify "Harper LSP server enabled"
   end
-
-  local client = clients[1]
-  local ns = vim.lsp.diagnostic.get_namespace(client.id)
-  virtual_text_enabled = not virtual_text_enabled
-  vim.diagnostic.config({ virtual_text = virtual_text_enabled }, ns)
-  vim.notify("Harper LSP diagnostics " .. (virtual_text_enabled and "enabled" or "disabled"))
-
-end, { desc = "Toggle Harper LSP diagnostics virtual text" })
+end, { desc = "Toggle Harper LSP server on/off" })
 
 return {
   cmd = { "harper-ls", "--stdio" },
@@ -41,7 +43,7 @@ return {
       diagnosticSeverity = "hint",
 
       linters = { -- See https://writewithharper.com/docs/rules
-        SpellCheck = false,
+        SpellCheck = true,
         SpelledNumbers = false,
         AnA = true,
         LongSentences = false,
