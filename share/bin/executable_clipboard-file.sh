4364fea29e088b34f580bc562c4c9bdb0f87d5cb
Stuart Rackham <srackham@gmail.com>
2026-08-29 08:41:10 +1200

    clipboard-file.sh: add cat command

--- share/bin/executable_clipboard-file.sh
+++ share/bin/executable_clipboard-file.sh
@@ -12,6 +12,8 @@
 #     write       Writes the text from the system clipboard to the clipboard file
 #     append      Appends the text starting on a new line from the system
 #                 clipboard to the clipboard file
+#     cat         Reads the text from the clipboard file to the system clipboard
+#                 and writes it to stdout
 
 set -e
 
@@ -19,7 +21,7 @@ CLIPBOARD_FILE="$HOME/vboxsf/clipboard.txt"
 
 usage() {
     echo "Usage: $(basename "$0") COMMAND" >&2
-    echo "Commands: read, write, append" >&2
+    echo "Commands: read, write, append, cat" >&2
     exit 1
 }
 
@@ -35,6 +37,10 @@ case "$1" in
     append)
         wl-paste >>"$CLIPBOARD_FILE"
         printf '\n' >>"$CLIPBOARD_FILE"
+        ;;
+    cat)
+        wl-copy <"$CLIPBOARD_FILE"
+        cat "$CLIPBOARD_FILE"
         ;;
     *)
         usage
